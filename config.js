e0ec533405bd8f97fbd5340c34ac8a4f03aff608
Ahmad1426r <riaz1426rr@gmail.com>
2025-01-14 21:58:57 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "OAByGZyA#DaWepxJVlQYquMV9giTqSPMtvP6FO5n8fh8Ug7VCN9Y",
     CAPTION: process.env.CAPTION || "POWERED BY AWANZADA",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
